09269ce65f86110bb5450cdaee8759fa25b00993
chanboklee <chanboklee@naver.com>
2023-09-10 21:21:19 +0900

    TEST : 회원 전체 조회 테스트

--- src/test/java/com/lee/shop/domain/MemberRepositoryTest.java
+++ src/test/java/com/lee/shop/domain/MemberRepositoryTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
@@ -67,6 +69,37 @@ class MemberRepositoryTest {
         Assertions.assertNotEquals(member.getPassword(), findMember.getPassword());
         Assertions.assertEquals(findMember.getPassword(), "4321");
         Assertions.assertEquals(member.getPassword(), "1234");
+
+    }
+
+    @Test
+    @DisplayName("회원 전체 조회 테스트")
+    void 회원전체_조회_테스트(){
+        // given
+        Member member1 = Member.builder().email("chanboklee@naver.com")
+                .password("1234")
+                .build();
+
+        Member member2 = Member.builder().email("test@naver.com")
+                .password("1234")
+                .build();
+
+        Member member3 = Member.builder().email("hong@daum.net")
+                .password("4321")
+                .build();
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        memberRepository.save(member3);
+
+        // when
+
+        List<Member> memberList = memberRepository.findAll();
+
+        // then
+        Assertions.assertEquals(memberList.get(0).getEmail(), "chanboklee@naver.com");
+        Assertions.assertEquals(memberList.size(), 3);
+        Assertions.assertEquals(memberList.get(2).getPassword(), "4321");
 
     }
 }
