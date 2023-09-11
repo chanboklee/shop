af198d52c15425339be00c66537fd194e88ed7da
chanboklee <chanboklee@naver.com>
2023-09-12 08:08:15 +0900

    FEAT : 회원, 배송지 연관관계 매핑

--- src/main/java/com/lee/shop/domain/Member.java
+++ src/main/java/com/lee/shop/domain/Member.java
@@ -1,13 +1,13 @@
 package com.lee.shop.domain;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
@@ -20,6 +20,9 @@ public class Member {
     private Long id;
     private String email;
     private String password;
+
+    @OneToMany(mappedBy = "member")
+    private List<DeliveryInfo> deliveryInfos = new ArrayList<>();
 
     @Builder
     public Member(String email, String password){
