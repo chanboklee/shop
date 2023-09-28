4cbd68d39b1882fdc1e12e4b8559d791a653aca5
chanboklee <chanboklee@naver.com>
2023-09-28 17:12:52 +0900

    DOCS : api 그룹화

--- src/main/java/com/lee/shop/config/SwaggerConfig.java
+++ src/main/java/com/lee/shop/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.lee.shop.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
@@ -13,5 +15,19 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SwaggerConfig {
 
+        @Bean
+        public GroupedOpenApi allApi(){
+                return GroupedOpenApi.builder()
+                        .group("전체")
+                        .pathsToMatch("/**")
+                        .build();
+        }
 
+        @Bean
+        public GroupedOpenApi memberApi(){
+                return GroupedOpenApi.builder()
+                        .group("회원")
+                        .pathsToMatch("/api/members/**")
+                        .build();
+        }
 }
