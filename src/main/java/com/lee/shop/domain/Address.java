c2e98361547d49e816c8e77eba3845718a6a84a8
chanboklee <chanboklee@naver.com>
2023-09-29 22:38:37 +0900

    FEAT : Address builder 추가

--- src/main/java/com/lee/shop/domain/Address.java
+++ src/main/java/com/lee/shop/domain/Address.java
@@ -1,8 +1,12 @@
 package com.lee.shop.domain;
 
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Embeddable
 public class Address {
@@ -10,4 +14,11 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    @Builder
+    public Address(String city, String street, String zipcode){
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
