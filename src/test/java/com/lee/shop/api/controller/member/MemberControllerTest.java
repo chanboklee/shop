17193487221a45014602598b9147bc9b63bc0e16
chanboklee <chanboklee@naver.com>
2023-11-06 21:41:00 +0900

    test : findMember - 회원 단건 조회 controller 테스트 작성

--- src/test/java/com/lee/shop/api/controller/member/MemberControllerTest.java
+++ src/test/java/com/lee/shop/api/controller/member/MemberControllerTest.java
@@ -4,8 +4,10 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.lee.shop.api.service.member.MemberService;
 import com.lee.shop.api.service.member.request.MemberSaveServiceRequest;
+import com.lee.shop.api.service.member.response.MemberSaveResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -15,6 +17,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.mockito.BDDMockito.given;
 
 @ActiveProfiles("test")
 @WebMvcTest(MemberController.class)
@@ -60,10 +64,30 @@ class MemberControllerTest {
 
         // when // then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/members")
-                        .content(objectMapper.writeValueAsString(memberSaveServiceRequest))
                         .contentType(MediaType.APPLICATION_JSON)
                 )
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk());
+    }
+
+    @DisplayName("회원 ID를 기준으로 회원을 조회한다.")
+    @Test
+    void findMember() throws Exception {
+        // given
+        MemberSaveServiceRequest memberSaveServiceRequest = MemberSaveServiceRequest.builder()
+                .name("이찬복")
+                .email("chanboklee@naver.com")
+                .password("1234")
+                .build();
+
+        MemberSaveResponse memberSaveResponse = memberService.saveMember(memberSaveServiceRequest);
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/members/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
     }
 }
