aeefd4ecd16bd65746ab3bfc7c0784005512a00d
chanboklee <chanboklee@naver.com>
2023-11-20 21:54:22 +0900

    feat : validation 추가

--- src/main/java/com/lee/shop/api/controller/member/request/MemberSaveRequest.java
+++ src/main/java/com/lee/shop/api/controller/member/request/MemberSaveRequest.java
@@ -1,14 +1,30 @@
 package com.lee.shop.api.controller.member.request;
 
 import com.lee.shop.api.service.member.request.MemberSaveServiceRequest;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
 @Getter
 public class MemberSaveRequest {
 
+    @NotBlank(message = "이름은 필수입니다.")
     private String name;
+
+    @NotBlank(message = "이메일은 필수입니다.")
     private String email;
+
+    @NotBlank(message = "패스워드는 필수입니다.")
     private String password;
+
+    @Builder
+    private MemberSaveRequest(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
 
     public MemberSaveServiceRequest toServiceRequest(){
         return MemberSaveServiceRequest.builder()
