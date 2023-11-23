4c41e14328723c8b62c51e81e5a2902f9614215a
chanboklee <chanboklee@naver.com>
2023-11-24 00:16:47 +0900

    test : 판매상품 리스트 조회 테스트

--- src/test/java/com/lee/shop/api/service/product/ProductServiceTest.java
+++ src/test/java/com/lee/shop/api/service/product/ProductServiceTest.java
@@ -2,15 +2,22 @@ package com.lee.shop.api.service.product;
 
 import com.lee.shop.api.service.product.request.ProductCreateServiceRequest;
 import com.lee.shop.api.service.product.response.ProductResponse;
+import com.lee.shop.domain.product.Product;
 import com.lee.shop.domain.product.ProductRepository;
+import com.lee.shop.domain.product.ProductSellingStatus;
+import com.lee.shop.domain.product.ProductType;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static com.lee.shop.domain.product.ProductType.TOP;
+import java.util.List;
+
+import static com.lee.shop.domain.product.ProductSellingStatus.*;
+import static com.lee.shop.domain.product.ProductType.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.groups.Tuple.tuple;
 
 @SpringBootTest
 class ProductServiceTest {
@@ -42,5 +49,41 @@ class ProductServiceTest {
         assertThat(productResponse)
                 .extracting("name", "price", "stockQuantity", "productType")
                 .contains("맨투맨", 16000, 100, TOP);
+    }
+    
+    @DisplayName("상품상태가 판매중, 판매보류인 상품 리스트만 조회한다.")
+    @Test
+    void getSellingProducts(){
+        // given
+        Product product1 = saveProduct("001", "나이키 맨투맨", 25000, 100, TOP, SELLING);
+        Product product3 = saveProduct("002", "귀걸이", 40000, 20, ACCESSORIES, STOP_SELLING);
+        Product product4 = saveProduct("003", "아디다스 패딩", 120000, 82, ACCESSORIES, HOLD);
+        Product product2 = saveProduct("004", "청바지", 12000, 50, BOTTOM, SELLING);
+
+
+        productRepository.saveAll(List.of(product1, product2, product3, product4));
+
+        // when
+        List<Product> products = productRepository.findAllByProductSellingStatusIn(forDisplay());
+
+        // then
+        assertThat(products)
+                .extracting("productNumber", "name", "price", "stockQuantity", "productType", "productSellingStatus")
+                .containsExactlyInAnyOrder(
+                        tuple("001", "나이키 맨투맨", 25000, 100, TOP, SELLING),
+                        tuple("004", "청바지", 12000, 50, BOTTOM, SELLING),
+                        tuple("003", "아디다스 패딩", 120000, 82, ACCESSORIES, HOLD)
+                );
+    }
+
+    private Product saveProduct(String productNumber, String name, int price, int stockQuantity, ProductType productType, ProductSellingStatus productSellingStatus) {
+        return Product.builder()
+                .productNumber(productNumber)
+                .name(name)
+                .price(price)
+                .stockQuantity(stockQuantity)
+                .productType(productType)
+                .productSellingStatus(productSellingStatus)
+                .build();
     }
 }
