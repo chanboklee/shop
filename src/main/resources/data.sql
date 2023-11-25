fa94a9d6febb04cf5a6bd4e55afbfd2b94847650
chanboklee <chanboklee@naver.com>
2023-11-25 20:24:02 +0900

    feat : 스프링 기동 후 테스트 데이터 추가 (profile -> local 인 경우만)

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into member(name, email, password)
+values ('이찬복', 'chanboklee@naver.com', '1234'),
+       ('홍길동', 'gildonghong@naver.com', '1234'),
+       ('춘향이', 'chun@yahoo.co.kr', '1234');
+
+insert into product(product_number, product_type, product_selling_status, name, price, stock_quantity)
+values ('001', 'TOP', 'SELLING', '나이키 맨투맨', 25000, 50),
+       ('002', 'BOTTOM', 'HOLD', '청바지', 45000, 12),
+       ('003', 'TOP', 'STOP_SELLING', '코트', 135000, 8);
