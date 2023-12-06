9b7d484bf75b8170d46652b43a989a3dbd1964f6
chanboklee <chanboklee@naver.com>
2023-12-06 21:49:36 +0900

    feat : @Valid 추가, 주문 리스트는 필수입니다.

--- src/main/java/com/lee/shop/api/controller/order/request/OrderCreateRequest.java
+++ src/main/java/com/lee/shop/api/controller/order/request/OrderCreateRequest.java
@@ -1,14 +1,18 @@
 package com.lee.shop.api.controller.order.request;
 
 import com.lee.shop.api.service.order.request.OrderCreateServiceRequest;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@NoArgsConstructor
 @Getter
 public class OrderCreateRequest {
 
+    @NotEmpty(message = "주문 리스트는 필수입니다.")
     List<OrderProductRequest> orderProductRequests;
 
     @Builder
