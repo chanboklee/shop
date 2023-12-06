c210ca2e5298cd1ca4a4c17181b490fa51b554d3
chanboklee <chanboklee@naver.com>
2023-12-06 21:49:56 +0900

    test : 상품 리스트가 비어있을 시 주문 생성 테스트 작성

--- src/test/java/com/lee/shop/api/controller/order/OrderControllerTest.java
+++ src/test/java/com/lee/shop/api/controller/order/OrderControllerTest.java
@@ -43,14 +43,35 @@ class OrderControllerTest {
 
         // when // then
         mockMvc.perform(MockMvcRequestBuilders.post("/api/members/{memberId}/orders", anyLong())
-                .content(objectMapper.writeValueAsString(orderCreateRequest))
-                .contentType(MediaType.APPLICATION_JSON)
-        )
+                        .content(objectMapper.writeValueAsString(orderCreateRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
                 .andDo(MockMvcResultHandlers.print())
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(jsonPath("$.code").value("200"))
                 .andExpect(jsonPath("$.httpStatus").value("OK"))
                 .andExpect(jsonPath("$.message").value("OK"))
                 .andExpect(jsonPath("$.data").isArray());
+    }
+
+    @DisplayName("신규 주문을 등록할 때 상품리스트는 1개 이상이어야 한다.")
+    @Test
+    void saveOrderWithEmptyOrderProducts() throws Exception {
+        // given
+        List<OrderProductRequest> orderProductRequestList = List.of();
+        OrderCreateRequest orderCreateRequest = OrderCreateRequest.builder().orderProductRequests(orderProductRequestList).build();
+
+        // when // then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/members/{memberId}/orders", 1L)
+                        .content(objectMapper.writeValueAsString(orderCreateRequest))
+                        .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(MockMvcResultHandlers.print())
+                .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                .andExpect(jsonPath("$.code").value("400"))
+                .andExpect(jsonPath("$.httpStatus").value("BAD_REQUEST"))
+                .andExpect(jsonPath("$.message").value("주문 리스트는 필수입니다."))
+                .andExpect(jsonPath("$.data").isEmpty());
+
     }
 }
